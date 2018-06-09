718f4c440c36bffccb7ddcb154f1483fd20f4504
bifot <bifot@bifot.ru>
2018-06-09 16:28:54 +0300

    rewrited on es6 class

--- index.js
+++ index.js
@@ -1,29 +1,38 @@
-module.exports = (schema) => (ctx, next) => {
-  const { body } = ctx.request
-  const errors = []
+class Validator {
+  static isValueValid (value, type) {
+    if (type === 'array') {
+      return Array.isArray(value)
+    }
 
-  for (const [ key, type ] of Object.entries(schema)) {
-    if (!body[key]) {
-      errors.push(`${key} must be ${type}`)
-    } else {
-      if (type === 'array') {
-        if (!Array.isArray(body[key])) {
-          errors.push(`${key} must be ${type}`)
-        }
-      } else {
-        if (typeof body[key] !== type) {
-          errors.push(`${key} must be ${type}`)
-        }
+    return typeof value === type
+  }
+
+  static validate (body, schema) {
+    const errors = []
+
+    for (const [ key, type ] of Object.entries(schema)) {
+      if (!this.isValueValid(body[key], type)) {
+        errors.push(`${key} must be ${type}`)
       }
     }
+
+    return errors
   }
 
-  if (errors.length) {
-    ctx.status = 400
-    ctx.body = { error: errors }
+  static middleware (schema) {
+    return (ctx, next) => {
+      const errors = Validator.validate(ctx.request.body, schema)
 
-    return false
+      if (errors.length) {
+        ctx.status = 400
+        ctx.body = { error: errors }
+
+        return false
+      }
+
+      return next()
+    }
   }
-
-  return next()
 }
+
+module.exports = Validator.middleware
