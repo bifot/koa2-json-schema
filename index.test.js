9f453e9fbc41ffff53b3f6d0ec649eb902a3e40f
bifot <bifot@bifot.ru>
2018-06-09 16:14:02 +0300

    refactored tests

--- index.test.js
+++ index.test.js
@@ -1,42 +1,56 @@
 const { expect } = require('chai')
 const jsonValidator = require('./')
 
+const applyMiddleware = (middleware, body) => {
+  const ctx = {
+    request: {
+      body
+    }
+  }
+
+  return {
+    next: middleware(ctx, () => true),
+    ctx
+  }
+}
+
 describe('koa json schema', () => {
   it('send body with correct data', () => {
-    const response = jsonValidator({
-      name: 'string',
-      phone: 'number',
-      hobbies: 'array'
-    })({
-      request: {
-        body: {
-          name: 'Mikhail',
-          phone: 79959978504,
-          hobbies: [
-            'Football',
-            'Basketball'
-          ]
-        }
+    const { next } = applyMiddleware(
+      jsonValidator({
+        name: 'string',
+        phone: 'number',
+        hobbies: 'array'
+      }),
+      {
+        name: 'Mikhail',
+        phone: 79959978504,
+        hobbies: [
+          'Football',
+          'Basketball'
+        ]
       }
-    }, () => true)
+    )
 
-    expect(response).to.equal(true)
+    expect(next).to.equal(true)
   })
 
   it('send body with incorrect data', () => {
-    const ctx = {
-      request: {
-        body: {
-          hobbies: 'football'
-        }
+    const { ctx, next } = applyMiddleware(
+      jsonValidator({
+        hobbies: 'array',
+        name: 'string'
+      }),
+      {
+        hobbies: 'football'
       }
-    }
-    const response = jsonValidator({
-      hobbies: 'array',
-      name: 'string'
-    })(ctx, () => true)
+    )
 
     expect(ctx.status).to.equal(400)
-    expect(ctx.body.error).to.be.a('array').to.have.length(2)
+    expect(ctx.body.error).to.deep.equal([
+      'hobbies must be array',
+      'name must be string'
+    ])
+    expect(next).to.equal(false)
   })
 })
